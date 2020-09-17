c0106d6263cbb8ac015c35570abd5c7210fe883f
Albahtross <alexliu401@gmail.com>
2020-09-17 02:31:14 -0400

    Implement button need to center

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView hello_text = (TextView)findViewById(R.id.updatable_text);
+        Button update_text_button = findViewById(R.id.update_text_button);
+
+        final String new_text = "You have clicked the button.";
+
+        update_text_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                hello_text.setText(new_text);
+            }
+        });
     }
+
+
 }
